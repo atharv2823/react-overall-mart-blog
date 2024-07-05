9592ff1309437ea4c49e50c491d785f4c458d061
athav2823 <atharvaneware@gmail.com>
2024-07-05 16:34:35 +0530

    path change

--- src/components/martcards/martcards.js
+++ src/components/martcards/martcards.js
@@ -1,7 +1,7 @@
 import "./MartCards.css"
 import { Link } from "react-router-dom"
 
-export default function MartCards( {id,title,specification,description,image}) {
+export default function MartCards( {id,title,image}) {
     return (
         <Link className="mart-card" to={`/martcards/${id}`}>
              <p>
